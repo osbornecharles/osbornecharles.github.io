189ac8525ae8b43ac9a08586f086376af09109b1
Charles Osborne <cosborne0731@gmail.com>
2024-09-05 16:43:42 +0800

    the point of this is to set up a more normal git workflow

---
+++ src/test.js
@@ -0,0 +1,14 @@
+export default alerter;
+export { alert2 };
+
+function alerter() {
+  alert('clicked button');
+}
+
+function alert2() {
+  alert('clicked button 2');
+}
+
+export function alert3() {
+  alert('clicked button 3');
+}
